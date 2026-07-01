a9b39c4535e18d888415b5de84fffb06af557d26
wesllyt20 <raphael.09@hotmail.es>
2026-07-01 10:15:59 -0500

    Refactor code structure for improved readability and maintainability

--- src/components/organisms/SiteHeader.vue
+++ src/components/organisms/SiteHeader.vue
@@ -28,11 +28,13 @@ function smoothScroll(e, href) {
       <!-- Left logos -->
       <div class="flex items-center gap-4">
         <img :src="y > 40 ? `${$baseUrl}logos/minam.png` : `${$baseUrl}logos/dark-minam.webp`" alt="Ministerio del Ambiente" width="120" height="36"
-          class="h-9 w-44 sm:w-auto" />
+          class="h-9 w-44 sm:w-auto hidden sm:block" />
+          <img :src="y > 40 ? `${$baseUrl}logos/responsive_minam.png` : `${$baseUrl}logos/dark_responsive_minam.png`" alt="Ministerio del Ambiente" width="120" height="36"
+          class="h-9 w-fit sm:w-auto sm:hidden" />
         <img :src="y > 40 ? `${$baseUrl}logos/igp.png` : `${$baseUrl}logos/dark-igp.webp`" alt="Instituto Geofísico del Perú" width="120" height="36"
           class="hidden sm:block h-9 w-auto" />
         <img :src="y > 40 ? `${$baseUrl}logos/responsive_igp.png` : `${$baseUrl}logos/dark_responsive_igp.png`" alt="IGP" class="sm:h-10 h-8 sm:hidden" />
-        <img :src="y > 40 ? `${$baseUrl}logos/aniversario.png` : `${$baseUrl}logos/dark_aniversario.png`" alt="Aniversario" class="sm:h-10 h-8 sm:hidden" />
+        <img :src="y > 40 ? `${$baseUrl}logos/aniversario.png` : `${$baseUrl}logos/dark_aniversario.png`" alt="Aniversario" class="sm:h-10 h-8" />
       </div>
 
       <!-- Desktop nav -->
